6e4d5182029d398ce6540c66514099941719d48b
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2024-09-19 12:48:04 -0500

    Add express and a ping route.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello there.");
+const express = require("express");
+const PORT = 3000;
+const app = express();
+
+app.get("/api/ping", (request, response) => {
+  try {
+    console.log(`Received ping request.`);
+    response.status(200).send("Grocery app Ping: successful");
+  } catch (error) {
+    console.log(`Error with ping request: ${error}`);
+    response.status(500).send("Grocery app Ping: failed", error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
+});
