06d7e1fb35b910eb511c0961cfc903d918bad14a
Vinay Gahlawat <vinay.gahlawat@gmail.com>
2024-10-06 13:39:34 -0500

    Add initial GET, POST and DELETE routes to API server.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const PORT = 3000;
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/ping", (request, response) => {
   try {
@@ -11,6 +12,51 @@ app.get("/api/ping", (request, response) => {
   } catch (error) {
     console.log(`Error with ping request: ${error}`);
     response.status(500).send("Grocery app Ping: failed", error);
+  }
+});
+
+let items = [];
+
+app.get("/api/items", (request, response) => {
+  try {
+    console.log(`Received item GET request...`);
+    response.status(200).send(items);
+  } catch (error) {
+    console.log(`Error with items GET request: ${error}`);
+    response.status(500).sendStatus("Items GET error", error);
+  }
+});
+
+app.post("/api/items", (request, response) => {
+  try {
+    console.log(`Received POST request...`);
+    if (!request.body) {
+      response.status(500).send("Missing body");
+    } else {
+      const newItem = request.body;
+      items.push(newItem);
+      response.status(200).send(items);
+    }
+  } catch (error) {
+    console.log(`Error with items POST request: ${error}`);
+    response.status(500).sendStatus("Items POST error", error);
+  }
+});
+
+app.delete("/api/items", (request, response) => {
+  try {
+    console.log(`Received DELETE request...`);
+    if (!request.body) {
+      response.status(500).send("Missing item name to delete");
+    } else {
+      const itemToDelete = request.body.name;
+      const index = items.findIndex((item) => item.name == itemToDelete);
+      items.splice(index, 1);
+      response.status(200).send();
+    }
+  } catch (error) {
+    console.log(`Error with items DELETE request: ${error}`);
+    response.status(500).sendStatus("Items DELETE error", error);
   }
 });
 
